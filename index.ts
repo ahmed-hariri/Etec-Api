185140d0b3a70d1c847ac9eab587ff0574496dfd
Ahmed Hariri <Ahmedhariri58@gmail.com>
2025-03-30 14:36:02 +0000

    Update CORS origins to include etec-dashboard

--- index.ts
+++ index.ts
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(helmet())
 app.use(cors({
-    origin: "*",
+    origin: ["http://localhost:3000", "https://etec-dashboard.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ['Content-Type'],
     credentials: true
